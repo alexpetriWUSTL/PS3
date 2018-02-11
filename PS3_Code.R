f0cf9858c23d7077115d7be75b98e9ffa2cc7660
alexpetriWUSTL <36051450+alexpetriWUSTL@users.noreply.github.com>
2018-02-11 14:44:01 -0600

    Complete s3 Method
    Completed code for my s3 method to choose a door.

--- PS3_Code.R
+++ PS3_Code.R
@@ -1,15 +1,23 @@
-doorNum <- c(1,2,3)
-q <- sample(doorNum, 1)
-q
-class(doorNum) <- "Door"
-PlayGame <- function(x,...){
+#s3
+
+doorNum <- c(1, 2, 3) #Write out the possible list of door numbers
+class(doorNum) <- "Door" #Assign doorNum to class Door
+PlayGame <- function(x,...){ #Write out the generic function
   UseMethod("PlayGame")
 }
-PlayGame.Door <- function(x){
-  if(identical(x, q) { 
+PlayGame.Door <- function(x){ #Write out my specific method
+  car <- sample(doorNum, 1) #Produce a sample number for where the car is
+  if(identical(x, car)) { #if the user picks the car door, they return a congrats statement
     print("Congratulations, you have chosen the correct door")
-  } else {
+  } else { #otherwise...
     print("Sorry, you have chosen the incorrect door")
   }
 }
-PlayGame.Door(3)
+PlayGame.Door(doorNum[[3]]) #call PlayGame.Door and choose your door number to see if you're correct!
+methods(class = "Door") #Use methods() to ensure PlayGame is in class "Door"
+
+
+
+
+
+#S4
