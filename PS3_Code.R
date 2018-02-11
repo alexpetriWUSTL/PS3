aa2d3b30b491ffd3fb0b4c97098e0934a00eabee
alexpetriWUSTL <36051450+alexpetriWUSTL@users.noreply.github.com>
2018-02-11 17:17:12 -0600

    Attempt at the s4 method
    I have my first method but it throws an error, going to office hours then reattempting at later date

--- PS3_Code.R
+++ PS3_Code.R
@@ -16,8 +16,48 @@ PlayGame.Door <- function(x){ #Write out my specific method
 PlayGame.Door(doorNum[[3]]) #call PlayGame.Door and choose your door number to see if you're correct!
 methods(class = "Door") #Use methods() to ensure PlayGame is in class "Door"
 
-
-
-
-
 #S4
+setClass(Class = "Door",
+    representation = representation(
+      x = "integer"
+    ),
+    prototype = prototype(
+      x = c()
+    )
+)
+
+setValidity("Door", function(object){
+  if(is.integer(object@x)){
+    return(TRUE)
+  } else {
+    return("The object is not an integer")
+  }
+}
+)
+
+
+setGeneric("PlayGame", def = function(object){
+  standardGeneric("PlayGame")
+  }
+)
+
+setMethod("PlayGame", signature("Door"), function(object){
+  car <- sample(1:3, 1)
+  if(identical(object@x, car)) { #if the user picks the car door, they return a congrats statement
+    print("Congratulations, you have chosen the correct door")
+    print(car)
+  } else { #otherwise...
+    print("Sorry, you have chosen the incorrect door")
+    print(car)
+  }
+}
+)
+doorNum <- new("Door", x = as.numeric(9))
+
+PlayGame(doorNum)
+
+
+
+
+
+
