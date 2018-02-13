0e91491d921262fe82a2b0c681a95dc1e9df4487
alexpetriWUSTL <36051450+alexpetriWUSTL@users.noreply.github.com>
2018-02-12 23:03:12 -0600

    Final Attempt at Problem Set 3
    Final code for problem set 3

--- PS3_Code.R
+++ PS3_Code.R
@@ -17,20 +17,18 @@ PlayGame.Door(doorNum[[3]]) #call PlayGame.Door and choose your door number to s
 methods(class = "Door") #Use methods() to ensure PlayGame is in class "Door"
 
 #S4
-setClass(Class = "Door", #use setclass to establish class "Door" with an integer vector x as an available slot
+setClass(Class = "Door", #use setclass to establish class "Door" with a numeric vector x as an available slot
     representation = representation(
-      x = "integer"
+      x = "numeric"
     ),
     prototype = prototype(
       x = c()
     )
 )
 
-setValidity("Door", function(object){ #set the validity so that objects in class door that aren't integers will return a fail statement
+setValidity("Door", function(object){ #set the validity so that objects in class door that are integer will return a fail statement
   if(is.integer(object@x)){
-    return(TRUE)
-  } else {
-    return("The object is not an integer")
+    return("The object is not a numeric")
   }
 }
 )
@@ -43,7 +41,7 @@ setGeneric("PlayGame", def = function(object){ #set my generic PlayGame function
 
 setMethod("PlayGame", signature("Door"), function(object){ #set the specific method
   car <- sample(1:3, 1) #a sample of 1-3 for where the car is located
-  if(identical(object@x, car)) { #if the user picks the car door, they return a congrats statement
+  if(identical(as.numeric(object@x), as.numeric(car))) { #if the user picks the car door, they return a congrats statement
     print("Congratulations, you have chosen the correct door")
     print(car)
   } else { #otherwise...
@@ -52,7 +50,7 @@ setMethod("PlayGame", signature("Door"), function(object){ #set the specific met
   }
 }
 )
-doorNum <- new("Door", x = as.numeric(9)) #create the doorNum that the player picks (insert numer in x=as.numeric())
+doorNum <- new("Door", x = as.numeric(3)) #create the doorNum that the player picks (insert numer in x=as.numeric())
 
 PlayGame(doorNum) #call the function
 
